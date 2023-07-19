e31ca93e485f7107a724cbbc53866e421a6457e2
ytw0728 <ytw0728@ytw0728-MacBookPro.local>
2023-07-19 21:48:01 +0900

    feat: tags utils

---
+++ packages/sfnt/src/constants/mod.rs
@@ -0,0 +1,6 @@
+/** 
+ * SFNT Version for TrueType and OpenType
+ * 
+ * @link: https://www.iana.org/assignments/media-types/font/sfnt
+ */
+pub const VERSION = 0x00010000
---
+++ packages/sfnt/src/reader/mod.rs
@@ -0,0 +1,11 @@
+struct DirectoryEntry {
+    pub uncompressed: bool
+    pub 
+}
+
+struct SFNTReader {
+    pub file,
+    pub check_checksums: number,
+    pub directory_entry: ,
+    pub sfntVersion: ,
+}
